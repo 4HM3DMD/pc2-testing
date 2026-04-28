3254540b21b17d1f5c5e486dea0121abd164af58
Irzhy Ranaivoarivony <irzhy@wau.co>
2026-04-28 17:28:11 +0300

    refactor: update metadata envelope to support flexible media protection schemas and multi-protection structures

--- pc2-node/data/test-apps/elacity-creator/app.js
+++ pc2-node/data/test-apps/elacity-creator/app.js
@@ -1179,12 +1179,26 @@
   function buildMetadataEnvelope(params) {
     var contentType = params.mimeType || 'application/octet-stream';
     var isPublic = params.accessMethod === 'free';
+    var isMediaFile = !!params.isMediaFile;
     var isResellable = params.accessMethod === 'buy_and_resell';
     var distributionLabel = params.accessMethod === 'buy_once' ? 'Buy Once'
       : params.accessMethod === 'free' ? 'Free' : 'Buy & Resell';
     var categories = Array.isArray(params.categories) ? params.categories
       : (params.category ? [params.category] : []);
     var tags = Array.isArray(params.tags) ? params.tags : [];
+
+    var protectionType = isMediaFile ? 'cenc:lit-aes-gcm-v1' : 'lit-aes-gcm-v1';
+    var protectionAlgorithm = isMediaFile ? 'aes-128' : 'aes-256-gcm';
+    var protections = isPublic ? [] : [{
+      algorithm: protectionAlgorithm,
+      protectionType: protectionType,
+      dataToEncryptHash: params.dataToEncryptHash,
+      actionCid: params.actionCid || '',
+      authority: params.authority || CONTRACTS.AUTHORITY_GATEWAY,
+      chain: 'base',
+      chainId: BASE_CHAIN_ID,
+      rpc: 'https://mainnet.base.org',
+    }];
 
     return {
       schema: 'https://raw.githubusercontent.com/Elacity/wiki/main/metadata/schemas/asset/v1.0/schema.json',
@@ -1200,9 +1214,7 @@
           mimeType: params.mimeType,
         }),
         object: 'self://content.json',
-        ...(!isPublic && {
-          protectionType: ['lit-aes-gcm-v1'],
-        }),
+        protectionType: isPublic ? [] : [protectionType],
         size: params.size,
       },
       asset: {
@@ -1210,14 +1222,7 @@
         mimeType: contentType,
         size: params.size,
         encrypted: !isPublic,
-        algorithm: isPublic ? 'none' : 'aes-256-gcm',
-        protectionType: isPublic ? 'none' : 'lit-aes-gcm-v1',
-        dataToEncryptHash: params.dataToEncryptHash,
-        actionCid: isPublic ? '' : (params.actionCid || ''),
-        authority: isPublic ? '' : (params.authority || CONTRACTS.AUTHORITY_GATEWAY),
-        chain: 'base',
-        chainId: BASE_CHAIN_ID,
-        rpc: 'https://mainnet.base.org',
+        protections: protections,
       },
       properties: {
         chainId: BASE_CHAIN_ID,
@@ -1241,7 +1246,7 @@
           { trait_type: 'Supply', value: params.copies || 10000 },
           { trait_type: 'Resell-Allowed', value: isResellable ? true : false },
           { trait_type: 'RRL-Percent', value: (isResellable ? (params.resellerCut || 900) : 0) / 10 },
-          { trait_type: 'Algorithm', value: params.algorithm || 'aes-256-gcm' }
+          { trait_type: 'Algorithm', value: protectionAlgorithm }
         ] : []),
         { trait_type: 'Adult', value: !!params.isAdult },
         { trait_type: 'Licensing', value: params.licensing },
@@ -1252,6 +1257,10 @@
 
   function buildContentJson(params) {
     var contentType = params.mimeType || 'application/octet-stream';
+    var protectionTypes = Array.isArray(params.protectionTypes)
+      ? params.protectionTypes.filter(function (t) { return typeof t === 'string' && t.length > 0; })
+      : [];
+    var isEncrypted = !!params.isEncrypted;
     return {
       schema: 'https://raw.githubusercontent.com/Elacity/wiki/main/metadata/schemas/content/v1.0/schema.json',
       version: '1.0',
@@ -1262,17 +1271,15 @@
       properties: {
         // keep this here for backward compatibility
         size: params.size,
-        protectionType: [
-          params.protectionType || 'lit-aes-gcm-v1'
-        ],
+        protectionType: protectionTypes,
         dataToEncryptHash: params.dataToEncryptHash,
         kid: params.kid || '',
       },
       attributes: [
         { trait_type: 'Content-Type', value: getContentTypeCode(params.mimeType) },
         { trait_type: 'Size', value: params.size },
-        { trait_type: 'Encrypted', value: true },
-        { trait_type: 'Algorithm', value: params.algorithm || 'aes-256-gcm' },
+        { trait_type: 'Encrypted', value: isEncrypted },
+        { trait_type: 'Algorithm', value: params.algorithm || (isEncrypted ? 'aes-256-gcm' : 'none') },
       ]
     };
   }
@@ -3235,6 +3242,7 @@
         channel: channel,
         authority: authorityAddress,
         image: imageUri,
+        isMediaFile: isMediaFile,
       };
 
       var envelope = buildMetadataEnvelope(metaParams);
@@ -3247,11 +3255,22 @@
 
       if (isMediaFile && mediaEncodeResult) {
         if (isFreeContent) {
-          envelope.asset.protectionType = 'none';
+          envelope.media.protectionType = [];
+          envelope.asset.protections = [];
           envelope.asset.cleartext = true;
           envelope.asset.directPlayback = true;
         } else {
-          envelope.asset.protectionType = 'cenc:web3-drm-v1';
+          envelope.media.protectionType = ['cenc:lit-aes-gcm-v1'];
+          envelope.asset.protections = [{
+            algorithm: 'aes-128',
+            protectionType: 'cenc:lit-aes-gcm-v1',
+            dataToEncryptHash: encryptResult.dataToEncryptHash || '',
+            actionCid: encryptResult.actionCid || '',
+            authority: authorityAddress,
+            chain: 'base',
+            chainId: BASE_CHAIN_ID,
+            rpc: 'https://mainnet.base.org',
+          }];
           envelope.asset.mpdUri = mediaEncodeResult.mpdUri;
           envelope.asset.kid = mediaEncodeResult.kid;
           envelope.asset.litBackend = 'chipotle';
@@ -3305,8 +3324,9 @@
         size: state.selectedFile.size,
         dataToEncryptHash: encryptResult.dataToEncryptHash,
         kid: kid,
-        protectionType: envelope.asset.protectionType,
-        algorithm: envelope.asset.algorithm,
+        protectionTypes: (envelope.media && Array.isArray(envelope.media.protectionType)) ? envelope.media.protectionType : [],
+        algorithm: (envelope.asset.protections && envelope.asset.protections[0] && envelope.asset.protections[0].algorithm) || (isFreeContent ? 'none' : 'aes-256-gcm'),
+        isEncrypted: !isFreeContent,
       });
 
       var contractJson = buildContractJson({
